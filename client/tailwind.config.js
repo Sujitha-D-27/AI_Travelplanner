e8eef37e20806c477e30e71c36ad8283404e8a98
Sujitha-D-27 <sujithadevaraj27@gmail.com>
2024-12-23 14:46:27 +0530

    git file changed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -55,3 +55,14 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 }
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+// 	content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+// 	theme: {
+// 	  extend: {},
+// 	},
+// 	plugins: [],
+//   }
+  
